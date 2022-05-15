59298bf38cc12a9e5aaa42dc33ed560c5af5d470
Guany <1084072690@qq.com>
2022-05-15 18:17:03 +0800

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,14 +7,15 @@ export default defineConfig({
       'meta',
       {
         name: 'viewport',
-        content: 'width=device-width,initial-scale=1,user-scalable=no'
+        content: 'width=device-width, initial-scale=1, user-scalable=no'
       }
     ],
     [
       'link',
       {
-        rel: 'icon',
-        href: '/favicon.ico'
+        rel: 'shortcut icon',
+        href: '/favicon.ico',
+        type: 'image/x-icon'
       }
     ],
     [
