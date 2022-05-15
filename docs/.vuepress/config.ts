2e0ea8aa33d152fc8c664b04748b8d89ad6c8ee6
Guany <1084072690@qq.com>
2022-05-15 18:22:07 +0800

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
       'meta',
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1, user-scalable=no'
+        content: 'width=device-width, initial-scale=1.0, user-scalable=no'
       }
     ],
     [
